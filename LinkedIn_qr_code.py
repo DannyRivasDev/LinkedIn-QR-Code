9c47c39614245e32a486042e1e40d5a90912dc20
Danny <dannyrivasdev@gmail.com>
2023-12-20 23:50:18 -0500

    WIP

--- LinkedIn_qr_code.py
+++ LinkedIn_qr_code.py
@@ -8,8 +8,8 @@ link = "Hello World!"
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
+    box_size=1,
+    border=1,
 )
 qr.add_data(link)
 qr.make(fit=True)
@@ -17,8 +17,15 @@ qr_img = qr.make_image(fill_color="black", back_color="white")
 
 background_img = Image.open("Background.png")
 
-qr_img = qr_img.resize(background_img.size, Image.ANTIALIAS)
+qr_position = (
+    (background_img.width // 4),
+    (background_img.height // 2)
+)
 
-merged_img = Image.alpha_composite(background_img.convert("RGBA"), qr_img.convert("RGBA"))
+qr_img = qr_img.resize(background_img.size, Image.Resampling.LANCZOS)
+
+merged_img = Image.new("RGBA", background_img.size, (0, 0, 0, 0))
+merged_img.paste(background_img.convert("RGBA"), (0, 0))
+merged_img.paste(qr_img.convert("RGBA"), qr_position, qr_img.convert("RGBA"))
 
 merged_img.save("myqrcode.png")
